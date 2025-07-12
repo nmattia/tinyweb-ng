dd6ad196396dfb8ccd47a16889fe90ff6539b131
Nicolas Mattia <nicolas@nmattia.com>
2025-07-12 23:45:11 +0200

    Simplify webapp

---
+++ examples/webapp.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env micropython
+
+import gc
+import json
+import sys
+from tinyweb.server import webserver
+import machine
+import logging
+import network
+
+
+def init_logger(name="root"):
+    rootLogger = logging.getLogger(name)
+    rootLogger.setLevel(logging.DEBUG)
+    for handler in rootLogger.handlers:
+        handler.setLevel(logging.DEBUG)
+
+
+init_logger("root")
+init_logger("WEB")
+
+server = webserver()
+
+
+@server.route("/", methods=["GET"])
+async def get_index(req, resp):
+    """Serve the webapp."""
+    await resp.send_file("/srv/index.html")
+
+
+@server.route("/info", methods=["GET"])
+async def get_info(req, resp):
+    """Returns information about the board."""
+    resp.headers[b"content-type"] = b"application/json"
+    await resp._send_headers()
+    obj = {
+        "mem_free": gc.mem_free(),
+        "mem_alloc": gc.mem_alloc(),
+        "platform": sys.platform,
+        "freq": machine.freq(),
+    }
+    await resp.send(json.dumps(obj))
+
+
+@server.route("/pins/", methods=["GET"])
+async def get_pins(req, resp):
+    """Returns a list of pins."""
+    from machine import Pin
+
+    pin_names: list[str] = []
+
+    for pin in dir(Pin.board):
+        if not pin.startswith("__"):
+            pin_names.append(pin)
+
+    resp.headers[b"content-type"] = b"application/json"
+    await resp._send_headers()
+    await resp.send(json.dumps({"pins": pin_names}))
+
+
+@server.route("/pins/<pin_name>/toggle", methods=["POST"])
+async def toggle_pin(req, resp, pin_name):
+    """Toggles any particular pin."""
+
+    from machine import Pin
+
+    pin = getattr(Pin.board, pin_name)
+    pin.toggle()
+
+    resp.headers[b"content-type"] = b"application/json"
+    await resp._send_headers()
+    await resp.send(json.dumps({}))
+
+
+def run():
+    # Set up AP
+    SSID = "tinyweb"
+    logging.info(f"creating AP with SSID '{SSID}'")
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(ssid="tinyweb", security=network.WLAN.SEC_OPEN)
+
+    (ip, _, _, _) = ap.ifconfig()
+    logging.info("AP configured: " + ip)
+
+    port = 8081
+    logging.info("starting server")
+    endpoint = f"http://{ip}:{port}"
+    logging.info(f"listening on '{endpoint}'")
+    server.run(host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    run()
